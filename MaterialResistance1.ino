177481bd5a552374464bf865618cc7d00f72ca7e
jcaf <firwar21@gmail.com>
2021-04-07 20:05:10 -0500

    2000

--- MaterialResistance1.ino
+++ MaterialResistance1.ino
@@ -148,7 +148,7 @@ int8_t send(float m, float v, float current)
     return 0;
 }
 
-#define VOLTS_NUM_SAMPLES 1000
+#define VOLTS_NUM_SAMPLES 2000
 float volts = 0.0f;
 float volts_acc = 0;
 float volts_media = 0;
