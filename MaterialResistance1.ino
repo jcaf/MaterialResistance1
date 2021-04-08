45ceac2e14c6f3551372320f03fd95fa1a7a3f19
jcaf <firwar21@gmail.com>
2021-04-07 20:10:42 -0500

    4096

--- MaterialResistance1.ino
+++ MaterialResistance1.ino
@@ -148,7 +148,7 @@ int8_t send(float m, float v, float current)
     return 0;
 }
 
-#define VOLTS_NUM_SAMPLES 2000
+#define VOLTS_NUM_SAMPLES 4096//2000
 float volts = 0.0f;
 float volts_acc = 0;
 float volts_media = 0;
@@ -187,6 +187,7 @@ void loop()
     	voltsCounterMedia = 0;
     	//
     	volts_media = (volts_acc/VOLTS_NUM_SAMPLES);
+    	//volts_media = (volts_acc>>);
     	volts_acc = 0;
 	}
 
