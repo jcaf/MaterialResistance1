330a8950a838a2768c3a832c8ebebd156f2a56a2
jcaf <firwar21@gmail.com>
2021-04-07 20:31:30 -0500

    v = v - 1.498;//1.5 center, elimino el offset

--- src/voltageMeas/voltageMeas.c
+++ src/voltageMeas/voltageMeas.c
@@ -22,7 +22,7 @@ float voltageMeas(void)
     v = ib16 * P_GAIN;//aqui v ya es voltaje
 
     //v = v - 1.5;//1.5 center, elimino el offset
-    v = v - 1.497;//1.5 center, elimino el offset
+    v = v - 1.498;//1.5 center, elimino el offset
     v = v * -1; //invierto la señal
     
     return v;
